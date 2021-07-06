71600af8f5ed800aff8c3b15635e9e3562b37a72
Serg <serg@example.com>
2021-07-06 13:49:09 +0300

    added nodejs examples

---
+++ _nodejs/typescript/build.sh
@@ -0,0 +1,8 @@
+# dev
+npm i
+docker compose up
+
+# prod
+./node_modules/.bin/tsc
+docker compose up
+
---
+++ _nodejs/winston-demo/build.sh
@@ -0,0 +1,12 @@
+# coping files
+docker build -t node-gm .
+
+docker run -it --rm --name gm node-gm bash 
+    node index.js 
+    ls out/
+    exit
+
+
+
+
+
